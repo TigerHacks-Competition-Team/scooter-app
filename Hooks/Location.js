424ece27cf5fcbc631d1d4395aafcc0687b2778f
VirtualMe64 <43835028+VirtualMe64@users.noreply.github.com>
2021-04-10 17:38:40 -0400

    Location is now distance based instead of time

--- Hooks/Location.js
+++ Hooks/Location.js
@@ -6,10 +6,11 @@ export default () => {
   const [result, setResult] = useState(null);
 
   useEffect(() => {
+    console.log("1234")
     let subscription = Location.watchPositionAsync(
-      { accuracy: Location.Accuracy.High, timeInterval: 10000, distanceInterval:0 },
+      { accuracy: Location.Accuracy.High, distanceInterval: 3 },
       (loc) => {
-        console.log(loc.coords.latitude)
+        console.log(`${loc.coords.latitude}, ${loc.coords.longitude}`)
         setLocation(loc)
       }
     )
