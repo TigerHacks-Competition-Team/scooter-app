63d109372b7eac441412f7d62f444cfa6b8d5112
aredhead1370 <71857488+aredhead1370@users.noreply.github.com>
2021-04-10 14:16:03 -0400

    Location Testing File

---
+++ LocationTesting.js
@@ -0,0 +1,38 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { DeviceMotion } from "expo-sensors";
+import { getGyroData, getLocation } from "./Hooks/index";
+import * as Permissions from "expo-permissions";
+
+const LocationTesting = () => {
+  const [gyroData] = getGyroData();
+  const [location, updateLocation] = getLocation();
+
+  return (
+    <View style={styles.container}>
+      <Text>
+        Rotation: {gyroData.rotation != null ? gyroData.rotation.alpha : 0}
+      </Text>
+      <Text>{location}</Text>
+      <Button
+        title="Update Location"
+        onPress={() => {
+          updateLocation();
+        }}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+export default LocationTesting
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
